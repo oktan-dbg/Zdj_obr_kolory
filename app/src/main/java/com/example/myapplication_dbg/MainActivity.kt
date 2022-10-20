a987b0c613d1dcbbb072906540f2edeb2c4805b1
Oktan <107392424+octan-nautil@users.noreply.github.com>
2022-10-20 07:30:43 +0200

    Pierwszy commit

---
+++ app/src/main/java/com/example/myapplication_dbg/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.myapplication_dbg
+
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
+import android.widget.*
+import androidx.core.app.ActivityCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
